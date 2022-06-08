c724fa07b31d021a4e37b54522c40026f56ca5f9
HiagoCS <hiago8cs@gmail.com>
2022-06-08 11:32:46 -0300

    updated foreign key of tb_transactions model

--- card-app/migrations/20220608142946-create-tb-transacoes.js
+++ card-app/migrations/20220608142946-create-tb-transacoes.js
@@ -9,13 +9,31 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       id_usuario: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references:{
+          model:'tb_usuarios',
+          key:'id'
+        },
+        onUpdate:'cascade',
+        onDelete:'cascade'
       },
       id_empresa: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references:{
+          model:'tb_empresas',
+          key:'id'
+        },
+        onUpdate:'cascade',
+        onDelete:'cascade'
       },
       id_produto: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references:{
+          model:'tb_rrodutos',
+          key:'id'
+        },
+        onUpdate:'cascade',
+        onDelete:'cascade'
       },
       createdAt: {
         allowNull: false,
