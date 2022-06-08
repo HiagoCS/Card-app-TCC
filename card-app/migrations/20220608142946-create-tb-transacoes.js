65b64e6b92795002471b057972c7a3e4dc95be2c
HiagoCS <hiago8cs@gmail.com>
2022-06-08 11:30:48 -0300

    created tb_transactions model

---
+++ card-app/migrations/20220608142946-create-tb-transacoes.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tb_transacoes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER
+      },
+      id_empresa: {
+        type: Sequelize.INTEGER
+      },
+      id_produto: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tb_transacoes');
+  }
+};
---
+++ card-app/models/tb_transacoes.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tb_transacoes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tb_transacoes.init({
+    id_usuario: DataTypes.INTEGER,
+    id_empresa: DataTypes.INTEGER,
+    id_produto: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'tb_transacoes',
+  });
+  return tb_transacoes;
+};
