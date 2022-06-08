cad45f5282cd1845529df890c01ac4748835e765
HiagoCS <hiago8cs@gmail.com>
2022-06-08 11:22:33 -0300

    created tb_users model

---
+++ card-app/migrations/20220608142058-create-tb-usuario.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tb_usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      senha: {
+        type: Sequelize.STRING
+      },
+      telefone: {
+        type: Sequelize.STRING
+      },
+      id_nivel: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:'tb_nivels',
+          key:'id'
+        },
+        onUpdate:'cascade',
+        onDelete:'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tb_usuarios');
+  }
+};
