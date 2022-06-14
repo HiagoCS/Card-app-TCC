bb3c3d721b60648ca40e30076d47884abdc8d24c
HiagoCS <hiago8cs@gmail.com>
2022-06-14 06:48:38 -0300

    created register user function in node controller and installed dotenv

--- card-app/Controller.js
+++ card-app/Controller.js
@@ -2,11 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const model = require('./models');
+require('dotenv').config();
 
 let app = express();
 app.use(cors({origin:'*'}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.post(process.env.REGISTERRT,async(req,res)=>{
+    let reqs = await model.tb_usuario.create({
+        'nome':req.body.nome,
+        'email':req.body.email,
+        'senha':req.body.senha,
+        'telefone':req.body.telefone,
+        'id_nivel':req.body.id_nivel,
+        'createdAt':new Date(),
+        'updateAt':new Date()
+    });
+    if(reqs){
+        res.send(JSON.stringify(`${req.body.nome} cadastrado com sucesso`));
+    }
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port, (req,res)=>{
